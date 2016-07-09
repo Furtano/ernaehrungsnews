fd167a9855183ff230ebbb68d518e6de6199e1ee
C. S <schady@posteo.de>
2016-07-09 16:20:35 +0200

    options page

--- ernaehrungsnews.php
+++ ernaehrungsnews.php
@@ -19,6 +19,9 @@ function hello_world_install() {
 /* Creates new database field */
 add_option("ernaehrungsnews_anzahl", '3', '', 'yes');
 add_option("ernaehrungsnews_trimmer", '100', '', 'yes');
+add_option("ernaehrungsnews_kategorieId", '3', '', 'yes');
+
+
 
 }
 
@@ -26,13 +29,15 @@ function hello_world_remove() {
 /* Deletes the database field */
 delete_option('ernaehrungsnews_anzahl');
 delete_option('ernaehrungsnews_trimmer');
+delete_option('ernaehrungsnews_kategorieId');
+
 }
 
 
-function hello_world_html_page() {
+function admin_html_menu() {
   ?>
   <div>
-    <h2>Hello World Options</h2>
+    <h2>Ernährungsnews Optionen</h2>
 
     <form method="post" action="options.php">
       <?php wp_nonce_field('update-options'); ?>
@@ -52,15 +57,21 @@ function hello_world_html_page() {
               value="<?php echo get_option('ernaehrungsnews_trimmer'); ?>" />
               </td>
             </tr>
+            <tr valign="top">
+              <th width="92" scope="row">Kategorie ID für News</th>
+              <td width="406">
+                <input name="ernaehrungsnews_kategorieId" type="text" id="ernaehrungsnews_kategorieId"
+                value="<?php echo get_option('ernaehrungsnews_kategorieId'); ?>" />
+                </td>
+              </tr>
         </table>
 
+        <input type="hidden" name="page_options" value="ernaehrungsnews_anzahl, ernaehrungsnews_trimmer, ernaehrungsnews_kategorieId" />
         <input type="hidden" name="action" value="update" />
-        <input type="hidden" name="page_options" value="ernaehrungsnews_anzahl" />
-        <input type="hidden" name="page_options" value="ernaehrungsnews_trimmer" />
 
 
         <p>
-          <input type="submit" value="<?php _e('Save Changes') ?>" />
+          <?php submit_button('Speichern') ?>
         </p>
 
       </form>
@@ -74,29 +85,17 @@ function hello_world_html_page() {
     add_action('admin_menu', 'hello_world_admin_menu');
 
     function hello_world_admin_menu() {
-      add_options_page('Hello World', 'Hello World', 'administrator',
-      'hello-world', 'hello_world_html_page');
+      add_options_page('Ernaehrungsnews', 'Ernaehrungsnews', 'administrator',
+      'Ernaehrungsnews', 'admin_html_menu');
     }
   }
-
-  function hello_world() {
-
-    $posts = get_latest_posts();
-    foreach ($posts as $post){
-      $postings .= '<div class="orangi">' . $post['post_title'] .' <span class="eatnewsdate">' . $post["post_date"] . "</span></div>";
-      $postings .= '<div class="eatnewsdesc"><a href="' . $post["guid"] .'">' . substr($post['post_content'],0,100) . "</a>...</div>";
-    }
-     return $postings;
-
-  }
-
   function get_latest_posts()
   {
 
     $args = array(
-      'numberposts' => 3,
+      'numberposts' => get_option('ernaehrungsnews_anzahl'),
       'offset' => 0,
-      'category' => 3,
+      'category' =>  get_option('ernaehrungsnews_kategorieId'),
       'orderby' => 'post_date',
       'order' => 'DESC',
       'post_type' => 'post',
@@ -106,4 +105,16 @@ function hello_world_html_page() {
       return wp_get_recent_posts( $args);
   }
 
-  add_shortcode( 'ernaehrungsnews', 'hello_world' );
+  function ernaehrungsnews() {
+
+    $posts = get_latest_posts();
+    foreach ($posts as $post){
+      $postings .= '<div class="orangi">' . $post['post_title'] .' <span class="eatnewsdate">' . $post["post_date"] . "</span></div>";
+      $postings .= '<div class="eatnewsdesc"><a href="' . $post["guid"] .'">' . substr($post['post_content'],0,get_option('ernaehrungsnews_trimmer')) . "</a>...</div>";
+    }
+     return $postings;
+
+  }
+
+
+  add_shortcode( 'ernaehrungsnews', 'ernaehrungsnews' );
